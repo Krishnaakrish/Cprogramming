b34ffa3d6d4a34695680dfaa7b0ecf58be735b81
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-22 23:26:35 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+int a,b,c;
+    
+scanf("%d %d %d", &a, &b, &c);
+
+    
+if (a>=b)
+    
+{
+        
+if(a>=c)
+            
+printf("a is the largest number.");
+        
+else
+            
+printf("c is the largest number.");
+    
+}
+    
+else
+    
+{
+        
+if(b>=c)
+            
+printf("b is the largest number.");
+        
+else
+            
+printf("c is the largest number.");
+    
+}
+    
+return 0;
+
+}
