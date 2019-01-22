ff29e5bff38b2e97eb3bd739601f9f6547935bec
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-22 23:22:19 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+
+
+int main()
+
+{
+
+char ch;
+  
+scanf("%c", &ch);
+  
+if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') )
+    
+printf("Alphabet");
+  
+else
+    
+printf("Not an Alphabet");
+  
+return 0;
+
+}
