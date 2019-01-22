0473639a7ffafc6012835ec6ba727dc4061a01d3
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-22 23:41:15 +0530

    Update Sum of array.c

--- Sum of array.c
+++ Sum of array.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
-void main()
-
-{
-    
-int a[20],i,k,sum = 0,n;
-    
-scanf("%d",&n);
-    
-for(i=0;i<n;i++)
-    
-scanf("%d",&a[i]);
-    
+main()
+{   
+int a[20],i,k,sum = 0,n;    
+scanf("%d",&n);    
 scanf("%d",&k);
-    
+for(i=0;i<n;i++)    
+scanf("%d",&a[i]);
 for(i=0; i<k;i++)
     
 {
