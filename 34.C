469a38301342f9105dcd4261b9e73cce960549fe
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:39:49 +0530

    Add files via upload

---
+++ 34.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int count_lines=1,i;
+ 
+char str[20];
+  
+gets(str);
+ 
+for(i=0; str[i]!=NULL; i++)
+ 
+{
+   
+if(str[i]=='.')
+     
+{
+   
+count_lines++;
+  
+}
+ 
+}
+ 
+printf("\n%d",count_lines);
+
+}
