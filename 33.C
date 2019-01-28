d5c0931388f9c2b8eb6fae4d94dcd00d6db012d2
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:39:31 +0530

    Add files via upload

---
+++ 33.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int count_space=0,i;
+ 
+char str[20];
+  
+gets(str);
+ 
+for(i=0; str[i]!=NULL; i++)
+ 
+{
+     
+if(str[i]==' ')
+     
+{
+   
+count_space++;
+  
+}
+ 
+}
+ 
+printf("\n%d",count_space);
+
+}
