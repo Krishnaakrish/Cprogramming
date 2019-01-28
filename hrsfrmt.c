3485dcd71291b209f904ab7437c531f05941a558
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:12:00 +0530

    Add files via upload

---
+++ hrsfrmt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int h1, m1,h2, m2, h3, m3;
+
+scanf("%d%d", &h1, &m1);
+
+scanf("%d%d", &h2, &m2);
+
+m3=(m1-m2);
+
+h3=(h1-h2);
+
+printf("%d\t%d",h3, m3);
+
+}
