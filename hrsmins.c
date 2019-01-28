2da260910a51552b03ad77b481407edcbfc33166
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:11:39 +0530

    Add files via upload

---
+++ hrsmins.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int time = 0;
+
+int minutes = 0;
+
+int hours = 0;
+
+int main()
+
+{
+
+int time;
+
+scanf("%d",&time);
+
+minutes = time % 60;
+
+hours = (time - minutes) / 60;
+
+printf("%d\t%d " ,hours, minutes);
+
+}
