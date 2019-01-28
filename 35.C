f5469a465d4e62fed4fee4c75546eacdf0cda40e
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:41:03 +0530

    Add files via upload

---
+++ 35.C
@@ -0,0 +1,34 @@
+
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+ 
+int c=0,j;
+ 
+char s[100];
+  
+gets(s);
+
+for(j=0; s[j]!=NULL; j++)
+ 
+{
+     
+if(s[j]=='0' || s[j]=='1'|| s[j]=='2'||
+        s[j]=='3'|| s[j]=='4'|| s[j]=='5'||
+        s[j]=='6'|| s[j]=='7'|| s[j]=='8'|| s[j]=='9')
+     
+{
+   
+c++;
+ 
+}
+ 
+}
+ 
+printf("\n%d",c);
+
+}
