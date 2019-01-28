84c5211ef562341a19792da51c9adb957b0e1e71
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:10:46 +0530

    Add files via upload

---
+++ numeric.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+char s[30];
+
+int i,flag=0,n=0;
+
+clrscr();
+
+scanf("%s",s);
+
+for(i=0;s[i]!='\0';i++)
+
+{
+
+if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
+
+{
+
+flag=flag+1;
+
+}
+
+}
+
+if(flag==0)
+
+printf("No");
+
+else
+
+printf("Yes");
+
+getch();
+
+}
