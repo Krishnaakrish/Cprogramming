867621ddd69dd61f0f582193a53321be25c8a017
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:41:26 +0530

    Add files via upload

---
+++ 36.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+ 
+int c=0,j;
+ 
+char str[100];
+  
+gets(str);
+ 
+for(j=0; str[j]!=NULL; j++)
+ 
+{
+     
+if(str[j]>='0' && str[j]<='9')
+            
+{}
+        
+else if((str[j]>='A' && str[j]<='Z')||(str[j]>='a' && str[j]<='z'))
+ {}
+        
+else if(str[j]==' ')
+            
+{}
+        
+else
+            
+c++;
+ 
+}
+ 
+printf("\n%d",c);
+
+}
---
+++ 37.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    
+int main()
+    
+{
+      
+int x, y, t;
+      
+scanf("%d%d", &x, &y);
+      
+t = x;
+     
+x = y;
+      
+y = t;
+  
+printf(" %d\t%d", x, y);
+    
+}
