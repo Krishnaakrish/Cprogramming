1937b3ea8f88e3784816eaa5b890b8f6fd378c2d
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-28 21:11:17 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+
+{
+    
+int n,a[100],i;
+    
+scanf("%d",&n);
+    
+for(i=0;i<n;i++)
+    
+scanf("%d",&a[i]);
+    
+for(i=0;i<n;i++)
+    
+printf("%d\t%d\n",a[i],i);
+
+}
