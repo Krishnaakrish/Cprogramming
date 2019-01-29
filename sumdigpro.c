5551de11c2b2c0fe6ceb615a92c6a97b6cd06d8b
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-01-29 15:15:12 +0530

    Create sumdigpro.c

---
+++ sumdigpro.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int fun(int n)
+{
+int rem,sum=0;
+while(n)
+{
+rem=n%10;
+sum=+=rem;
+n/=10;
+}
+return sum;
+}
+void main()
+{
+int n,i,k;
+scanf("%d",&n);
+if(n<800)
+{
+printf("INVALID");
+}
+else{
+k=800;
+for(i=0;k<n;i++)
+{
+k+=i;
+printf("%d ",fun(k));
+i++;
+}
+}
+}
+
