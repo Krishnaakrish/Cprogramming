dfbbf61220b5699db6c7b192f1e0a704af3c78fd
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-06 19:53:25 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+void main()
+
+{
+
+	char a[10];
+
+	scanf("%s",a);
+
+
+
+	int i,flag=0,m;
+
+	m=strlen(a);
+
+	for(i=0;i<m;i++)
+
+	{
+
+		if(a[i]=='1' || a[i]=='0')
+
+		{
+
+			flag=1;
+
+		}
+
+		else
+
+		{
+
+			flag=0;
+
+			break;
+
+		}
+
+		
+
+	}
+
+	if(flag==1)
+
+	{
+
+		printf("yes");
+
+	}
+
+	
+
+}
