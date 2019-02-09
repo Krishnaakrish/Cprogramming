1725936d8979b6e96ad7927b7edfb4bbd7a26c61
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-09 19:19:55 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    char a[30];
+    int i,n;
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+        printf("%c",a[i]);
+    }
+    printf(" ");
+    for(i=0;i<n;i++)
+    {
+        if(i%2==1)
+        printf("%c",a[i]);
+    }
+}
