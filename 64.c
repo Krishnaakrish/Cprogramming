633c16c408086dc8dbebd1fd023845a9b67d3a1f
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-09 18:34:30 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+void main()
+{
+	int num,even;
+	printf("enter the number:");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		even=num-2;	
+	}
+	else
+	{
+		even=num-1;
+	}
+	printf("\n%d",even);
+ 
+}
