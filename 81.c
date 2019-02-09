6a15f685f0a5e6518a618336da3ca68182de4c2e
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-09 18:58:14 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+    char string1[20];
+    int i, length;
+    int flag = 0;
+    scanf("%s", string1);
+    length = strlen(string1);
+    for(i=0;i < length ;i++){
+        if(string1[i] != string1[length-i-1]){
+            flag = 1;
+            break;
+   }
+}
+        if (flag) {
+        printf("no");
+    }    
+    else {
+        printf("yes");
+    }
+   
+}
