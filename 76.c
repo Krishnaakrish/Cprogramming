f00559d072bf512f8143f047cf45a33a1cce6e6f
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-09 18:22:46 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+int num,i,flag=0;
+
+scanf("%d",&num);
+
+for(i=2;i<=num/2;i++)
+
+{
+
+if(num%i==0)
+
+{
+
+flag=1;
+
+break;
+
+}
+
+}
+
+if(flag==0)
+
+{
+
+printf("\n prime");
+
+}
+
+else
+
+{
+
+printf("\n not a prime number");
+
+}
+
+
+
+}
