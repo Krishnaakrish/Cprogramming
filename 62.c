c2427c95e940f3f06a43e6a259a9f761773049f3
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-09 18:31:51 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+ 
+void main()
+{
+	int num;
+	printf("\n enter the number from 1 to 10");
+	scanf("%d",&num);
+	switch(num)
+	{
+		case 1:
+		printf("\none");
+		break;
+		case 2:
+		printf("\ntwo");
+		break;
+		case 3:
+		printf("\nthree");
+		break;
+		case 4:
+		printf("\nfour");
+		break;
+		case 5:
+		printf("\nfive");
+		break;
+		case 6:
+		printf("\nsix");
+		break;
+		case 7:
+		printf("\nseven");
+		break;
+		case 8:
+		printf("\neight");
+		break;
+		case 9:
+		printf("\nnine");
+		break;
+		case 10:
+		printf("\nten");
+		break;
+		default:
+		printf("\n enter numbers from 1 to 10");
+	}
+ 
+}
+ 
