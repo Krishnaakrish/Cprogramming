30da76fb380f60df55c1bb6bcac747b32469bf57
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-02-20 10:29:24 +0530

    Create 3p7.c

---
+++ 3p7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    char a[100],i;
+    gets(a);
+    for(i=0;a[i]!=NULL;i++)
+    {
+        if(a[i]>='A'&&a[i]<='Z')
+        a[i]+=32;
+        else if(a[i]>='a'&&a[i]<='z')
+        a[i]-=32;
+        }
+        puts(a);
+}
