fcfcee480d9bd1c6272d65d7b7d06f5859bf497a
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-03-10 20:17:58 +0530

    Create 8p5.c

---
+++ 8p5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() 
+{
+	int n,a[30],i,j,c=0;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+        for(j=i+1;j<n;j++)
+	        if(a[i]<a[j])
+		        c++;
+	printf("%d",c);
+}
