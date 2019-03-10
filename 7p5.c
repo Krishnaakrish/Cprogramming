eb3e2d69a756122c09dfae02c97cdd009549cf06
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-03-10 19:56:38 +0530

    Create 7p5.c

---
+++ 7p5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() 
+{
+	int n,a[20],i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+		for(i=0;i<n;i++)
+	    
+	        if(a[i]<n)
+	        printf("%d ",a[i]);
+	   
+}
