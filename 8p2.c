63b94393b68a0e8de6820936603acf16519a5f78
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-03-10 20:11:46 +0530

    Create 8p2.c

---
+++ 8p2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() 
+{
+	int n,a[30],i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	    if(a[i]>a[i+1])
+		{
+			printf("%d",a[i]);
+			break;
+		}
+	
+}
