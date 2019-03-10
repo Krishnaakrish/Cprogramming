8d76ae82e60202a7f7125f66acbea1de5c8390f2
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-03-10 19:55:35 +0530

    Create 7p4.c

---
+++ 7p4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+	int n,k,a[20],i,j,temp;
+	scanf("%d%d",&n,&k);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		if(a[i]>a[j])
+		{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+		}
+	}
+	for(i=0;i<n;i++)
+	   	        if(a[i]<k)
+	        printf("%d ",a[i]);
+	    
+}
