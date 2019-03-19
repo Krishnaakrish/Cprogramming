088c510fddd813df55b2949a5cd590d9875f9126
Krishnaakrish <36375276+Krishnaakrish@users.noreply.github.com>
2019-03-19 19:02:53 +0530

    Create 13p8.c

---
+++ 13p8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int a[100],i,s=1,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+           scanf("%d",&a[i]);
+    
+    for(i=0;i<n;i++)
+          s=s*a[i];
+  
+    if(s%2==0)
+         printf("even");
+   
+    else
+          printf("odd");
+  
+  }
